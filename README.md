a9887e21dfab33825ebc4edfbc6caf09472e67c5
Vlad Pavlyuchenko <vlad.pavl@icloud.com>
2020-10-23 20:45:38 +0300

    add full adaptive

---
+++ README.md
@@ -0,0 +1,6 @@
+<h1>How to use</h1>
+<ol>
+<li>Clone or Download from GitHub</li>
+<li>Install Node Modules: npm i</li>
+<li>Run: gulp</li>
+</ol>
--- app/blocks/components/main-slider/main-slider.js
+++ app/blocks/components/main-slider/main-slider.js
@@ -4,7 +4,7 @@ const playButtonsFirst = document.querySelector('.main-slider__play');
 const video = document.querySelector('video');
 
 Swiper.use([Navigation, EffectFade]);
-const swiperSlider2 = new Swiper('.main-slider', {
+const swiperSlider2 = new Swiper('.main-slider__container', {
   loop: true,
   slidesPerView: 1,
   centeredSlides: true,
@@ -19,29 +19,3 @@ const swiperSlider2 = new Swiper('.main-slider', {
   effect: 'fade',
   simulateTouch: false,
 });
-
-document.querySelector('.main-slider__play').addEventListener('click', function () {
-  video.play();
-});
-
-// playButtonsFirst.forEach((el) => {
-// 	el.addEventListener('click', (e) => {
-// 		let video = e.currentTarget.closest('.main-slider__media').querySelector('video');
-// 		video.play();
-// 		e.currentTarget.style.display = 'none';
-// 		setTimeout(() => {
-// 			video.volume = 0.5;
-// 		}, 1000);
-// 	});
-// });
-
-// swiperSlider2.on('transitionEnd', function () {
-// 	let videos = document.querySelectorAll('.preview__slider video');
-// 	videos.forEach((el) => {
-// 		el.pause();
-// 		el.currentTime = 0;
-// 	});
-// 	playButtonsFirst.forEach((el) => {
-// 		el.style.display = 'block';
-// 	});
-// });
